80024a5605c2736a2372e9d4cfa202d5fea77770
linemoller <line.moller@ess.eu>
2025-03-13 18:51:26 +0100

    add test for one dim array and three dim array

--- tests/test_ad00.py
+++ tests/test_ad00.py
@@ -83,6 +83,46 @@ class TestSerialisationAD00:
         assert entry.timestamp_ns == original_entry["timestamp_ns"]
         assert entry.data == original_entry["data"]
 
+    def test_serialises_and_deserialises_ad00_one_dim_array(self):
+        """
+        Round-trip to check what we serialise is what we get back.
+        """
+        original_entry = {
+            "source_name": "some other source name",
+            "unique_id": 789679,
+            "data": np.array([1, 2, 3, 4, 5, 6], dtype=np.float32),
+            "timestamp_ns": time.time_ns(),
+        }
+
+        buf = serialise_ad00(**original_entry)
+        entry = deserialise_ad00(buf)
+
+        assert entry.unique_id == original_entry["unique_id"]
+        assert entry.source_name == original_entry["source_name"]
+        assert entry.timestamp_ns == original_entry["timestamp_ns"]
+        assert np.array_equal(entry.data, original_entry["data"])
+        assert entry.data.dtype == original_entry["data"].dtype
+
+    def test_serialises_and_deserialises_ad00_three_dim_array(self):
+        """
+        Round-trip to check what we serialise is what we get back.
+        """
+        original_entry = {
+            "source_name": "some other source name",
+            "unique_id": 789679,
+            "data": np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]], dtype=np.float32),
+            "timestamp_ns": time.time_ns(),
+        }
+
+        buf = serialise_ad00(**original_entry)
+        entry = deserialise_ad00(buf)
+
+        assert entry.unique_id == original_entry["unique_id"]
+        assert entry.source_name == original_entry["source_name"]
+        assert entry.timestamp_ns == original_entry["timestamp_ns"]
+        assert np.array_equal(entry.data, original_entry["data"])
+        assert entry.data.dtype == original_entry["data"].dtype
+
     def test_if_buffer_has_wrong_id_then_throws(self):
         original_entry = {
             "source_name": "some source name",
